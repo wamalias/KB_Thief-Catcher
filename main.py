9739473e8df654345711fc0c1da9615bf35df363
Melanie Refman <87106838+melanierefman@users.noreply.github.com>
2023-05-14 22:15:33 +0700

    feat: update level

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame, sys
 from button import Button
+# from easy import *
+# from medium import *
+# from hard import *
 
 pygame.init()
 
@@ -8,23 +11,23 @@ pygame.display.set_caption("Menu")
 
 BG = pygame.image.load("assets/Background.png")
 
-def get_font(size): # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("assets/font.ttf", size)
-
 def play():
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
+        pygame.display.set_caption("Play")
+        
+        FILL = pygame.image.load("assets/Level.png")
 
-        SCREEN.fill("black")
-
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
-
-        PLAY_BACK = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 460))
-
-        PLAY_BACK.update(SCREEN)
-
+        SCREEN.blit(FILL, (0, 0))
+        
+        PLAY_BACK = Button(image=pygame.image.load("assets/x.png"), pos=(950, 50))
+        EASY_BUTTON = Button(image=pygame.image.load("assets/Easy Rect.png"), pos=(500, 370))
+        MEDIUM_BUTTON = Button(image=pygame.image.load("assets/Medium Rect.png"), pos=(500, 520))
+        HARD_BUTTON = Button(image=pygame.image.load("assets/Hard Rect.png"), pos=(500, 670)) 
+        
+        for button in [PLAY_BACK, EASY_BUTTON, MEDIUM_BUTTON, HARD_BUTTON]:
+            button.update(SCREEN)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -32,12 +35,21 @@ def play():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
                     main_menu()
+                if EASY_BUTTON.checkForInput(PLAY_MOUSE_POS):
+                    exec(open("easy.py").read())
+                if MEDIUM_BUTTON.checkForInput(PLAY_MOUSE_POS):
+                    exec(open("medium.py").read())
+                if HARD_BUTTON.checkForInput(PLAY_MOUSE_POS):
+                    exec(open("hard.py").read())
+                    # pygame.quit()
+                    # sys.exit()
 
         pygame.display.update()
     
 def about():
     while True:
         ABOUT_MOUSE_POS = pygame.mouse.get_pos()
+        pygame.display.set_caption("About")
         
         FILL = pygame.image.load("assets/About.png")
 
