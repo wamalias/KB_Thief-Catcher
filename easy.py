46f2bb337e154beb3a1d342c8716e170bff9f457
yusnaaaaa <91377793+yusnaaaaa@users.noreply.github.com>
2023-05-22 23:55:28 +0700

    feat: add comment

--- easy.py
+++ easy.py
@@ -9,40 +9,42 @@ import path
 import time
 # from main import *
 
-# Class for the orange dude
+# Class Player berfungsi untuk merepresentasikan karakter pemain dalam game
 class Player(object):
-    
+    # berfungsi untuk menginisialisasi atribut objek `Player` saat objek dibuat
     def __init__(self):
-        self.image_surf = pygame.image.load("img/detective.png").convert_alpha()
-        self.resized = pygame.transform.scale(self.image_surf, (55, 55))
+        self.image_surf = pygame.image.load("img/detective.png").convert_alpha() # menggambar karakter player
+        self.resized = pygame.transform.scale(self.image_surf, (55, 55)) # mengatur ukuran gambar karakter player
         self.x = 10
         self.y = 643
-        self.life = 1
- 
+        self.life = 1 # menyimpan jumlah nyawa karakter player
+    
+    # berfungsi untuk menggerakkan player pada sumbu x atau sumbu y
     def move(self, dx, dy):
         
-        # Move each axis separately. Note that this checks for collisions both times.
         if dx != 0:
             self.move_single_axis(dx, 0,)
         if dy != 0:
             self.move_single_axis(0, dy,)
     
+    # berfungsi untuk menggerakkan player pada sumbu yang ditentukan
     def move_single_axis(self, dx, dy):
-        
-        # Move the rect
+
         self.x += dx
         self.y += dy
         self._player_rect = pygame.Rect(self.x, self.y, self.resized.get_width(), self.resized.get_height())
         self._player_rect.center = (self.x, self.y)
- 
+
+# Class Enemy berfungsi untuk merepresentasikan musuh dalam game
 class Enemy(object):
-    
+    # berfungsi untuk menginisialisasi atribut objek`Enemy` saat objek dibuat
     def __init__(self):
         self.end_rect = pygame.image.load("img/thief.png").convert_alpha()
         self.resized = pygame.transform.scale(self.end_rect, (55, 55))
         self.x = 950
         self.y = 90
 
+# Class Wall berfungsi untuk merepresentasikan dinding dalam suatu game
 class Wall(object) :
     
     def __init__(self, img, x, y):
@@ -51,7 +53,8 @@ class Wall(object) :
         self.x = x
         self.y = y
         Walls.append(self)
-        
+ 
+# Class Road berfungsi untuk merepresentasikan jalan dalam suatu game
 class Road(object) :
     
     def __init__(self, img, x, y):
@@ -61,18 +64,20 @@ class Road(object) :
         self.y = y
         Roads.append(self)
 
+# Class Hint berfungsi untuk merepresentasikan hint dalam suatu game
 class Hint(object) :
     
     def __init__(self, img, x, y, index):
-        self.hint = pygame.image.load(img).convert()
-        self.resized = pygame.transform.scale(self.hint, (50, 50))
-        self.x = x
-        self.y = y
+        self.hint = pygame.image.load(img).convert() # mengambil gambar hint
+        self.resized = pygame.transform.scale(self.hint, (50, 50)) # mengatur ukuran gambar hint
+        self.x = x # posisi dalam koordinat x
+        self.y = y # posisi dalam koordinat y
         self.index = index
         self.question = 1
         Hints.append(self)
-        path.append(abs(self.x - 950), abs(self.y - 100), index)
-        
+        path.append(abs(self.x - 950), abs(self.y - 100), index) # menambahkan informasi perbedaan x dan y terhadap titik koordinat tertentu kedalam daftar path
+
+# berfungsi untuk memeriksa adanya tabrakan antara player dengan dinding dalam game
 def checkWall(player, Walls, maskP) :
     sign = 0
     for wall in Walls:
@@ -83,14 +88,15 @@ def checkWall(player, Walls, maskP) :
         
     return sign
 
+# berfungsi untuk memeriksa apakah player berinteraksi dengan hint dalam game
 def checkHint(player, Hints, maskP) :
     for hint in Hints:
         maskH = pygame.mask.from_surface(hint.resized)
-        offset = (hint.x - player.x, hint.y - player.y)
+        offset = (hint.x - player.x, hint.y - player.y) # menghitung perbedaan posisi antara player dengan hint saat ini
         if maskP.overlap(maskH, offset):
             if(hint.question == 1) : 
-                QUESTION = Questions()
-                correction = QUESTION.display(player)
+                QUESTION = Questions() # untuk menampilkan pertanyaan terkait hint pada player
+                correction = QUESTION.display(player) # mengembalikan koreksi jawaban yang diberikan oleh player
                 if correction == "Benar" : 
                     if hint.index != 'C' : 
                         next = path.find(hint.index, 'C')
@@ -108,7 +114,8 @@ def checkHint(player, Hints, maskP) :
                     final_page("img/lose.png", "Sorry, You Lose!")
                 hint.question = 0
             break
-                
+ 
+# berfungsi untuk menampilkan halaman akhir game
 def final_page(page, caption):
     pygame.display.set_caption(caption)
 
@@ -123,11 +130,13 @@ def final_page(page, caption):
                 sys.exit()
 
         pygame.display.update()
-        
+
+# class ImageSelector berfungsi untuk memilih path secara acak
 class ImageSelector:
     def __init__(self, folder_path):
         self.folder_path = folder_path
-    
+        
+    # untuk mendapatkan path secara acak
     def get_random_image_path(self):
         image_files = [f for f in os.listdir(self.folder_path) if os.path.isfile(os.path.join(self.folder_path, f))]
         if image_files:
@@ -139,13 +148,15 @@ import sys
 
 pygame.init()
 
+# Class Question berfungsi untuk pengimplementasian perntanyaan dalam game
 class Questions:
     def __init__(self):
         self.screen = pygame.display.set_mode((1000, 800))
         self.image_selector = ImageSelector("riddles")
         self.displayed_image_path = None
         self.font = pygame.font.Font(None, 36)
-
+    
+    # untuk menampilkan pertanyaan pada layar
     def display(self, player):
         while True:
             Q_MOUSE_POS = pygame.mouse.get_pos()
@@ -156,7 +167,8 @@ class Questions:
                 if self.displayed_image_path:
                     image = pygame.image.load(self.displayed_image_path)
                     self.screen.blit(image, (0, 0))
-
+                    
+            # untuk menampilkan pilihan jawaban A/B/C
             Q_BACK = Button(image=pygame.image.load("img/x.png"), pos=(950, 50))
             A_ANS = Button(image=pygame.image.load("img/A.png"), pos=(200, 700))
             B_ANS = Button(image=pygame.image.load("img/B.png"), pos=(500, 700))
@@ -169,12 +181,14 @@ class Questions:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
+                
+                # untuk menampilkan pertanyaan sesuai dengan letak jawaban benarnya
                 correct_paths_set_A = {"riddles/1.png", "riddles/5.png", "riddles/11.png", "riddles/12.png", "riddles/15.png", "riddles/17.png", "riddles/19.png"}
                 correct_paths_set_B = {"riddles/2.png", "riddles/4.png", "riddles/6.png", "riddles/8.png", "riddles/9.png", "riddles/10.png", "riddles/13.png", "riddles/18.png", "riddles/20.png"}
                 correct_paths_set_C = {"riddles/3.png", "riddles/7.png", "riddles/14.png", "riddles/16.png", "riddles/21.png"}
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
+                    # jawaban A
                     if A_ANS.checkForInput(Q_MOUSE_POS):
                         response = "salah"
                         for element in correct_paths_set_A:
@@ -193,7 +207,7 @@ class Questions:
 
                             else:
                                 return
-
+                    # jawaban B
                     if B_ANS.checkForInput(Q_MOUSE_POS):
                         response = "salah"
                         for element in correct_paths_set_B:
@@ -211,7 +225,7 @@ class Questions:
                                 player.life -= 1
                             else:
                                 return
-
+                    # jawaban C
                     if C_ANS.checkForInput(Q_MOUSE_POS):
                         response = "salah"
                         for element in correct_paths_set_C:
@@ -236,12 +250,14 @@ class Questions:
 
             pygame.display.update()
 
+# Class HintView untuk menampilkan tampilan hint dalam permainan
 class HintView:
     def __init__(self, path):
         self.screen = pygame.display.set_mode((1000, 800))
         self.image = pygame.image.load(path)
         self.font = pygame.font.Font(None, 36)
-
+    
+    # untuk menampilkan tampilan hint
     def display(self):
         while True:
             Q_MOUSE_POS = pygame.mouse.get_pos()
@@ -265,7 +281,8 @@ class HintView:
                         return
 
             pygame.display.update()
-           
+ 
+# untuk menampilkan timer pada layar dalam detik
 def display_timer(screen, elapsed_time):
     font = pygame.font.Font(None, 36)
     timer_text = font.render("Time: " + format_time(elapsed_time), True, (0, 0, 0))
@@ -277,7 +294,7 @@ def format_time(secs):
     time_format = "{:02d}:{:02d}".format(int(mins), int(secs))
     return time_format
 
-# Initialise pygame
+# untuk menginisialisasi library pygame dan mengatur posisi window ke tengah layar
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
  
@@ -285,13 +302,14 @@ pygame.init()
 pygame.display.set_caption("Help Detective to Catch the Thief!")
 screen = pygame.display.set_mode((1000, 800))
 
-clock = pygame.time.Clock()
-Walls = []
-Roads = []
-Hints = []
-player = Player() # Create the player
-enemy = Enemy()
-# Holds the level layout in a list of strings.
+clock = pygame.time.Clock() # untuk mengontrol kecepatan waktu game
+Walls = [] # untuk menyimpan objek dinding
+Roads = [] # untuk menyimpan objek jalan
+Hints = [] # untuk menyimpan objek hint
+player = Player() # membuat objek player dengan menggunakan class player
+enemy = Enemy() # membuat objek enemy dengan menggunakan class enemy
+
+# maps yang digunakan untuk level easy
 level = [
     "WWWWWWWWWWWWWWWWWPWW",
     "WPPPPPPPPPPPPPPPPBPP",
@@ -311,7 +329,7 @@ level = [
     "WWWWWWWWWWWWWWWWWWWW",
 ]
 
-# Parse the level string above. W = wall, E = exit
+# melakukan parsing dari string level yang diberikan. Setiap karakter dalam string level diperiksa dan objek yang sesuai dibuat berdasarkan karakter tersebut
 x = y = 0
 index = 'A'
 for row in level:
@@ -379,10 +397,12 @@ for row in level:
 path.roadMap()
 path.printG()
 
+# untuk memuat dan memainkan efek suara dalam game
 game_sound = SoundEffectGame()
 game_sound.load_sound_effect("play", "sounds/play.mp3")
 game_sound.play_sound_effect("play")
- 
+
+# set waktu 3 menit / 180 detik 
 MAX_PLAY_TIME = 180  # 3 minutes in seconds
 start_time = time.time()
 elapsed_time = 0
@@ -397,7 +417,7 @@ while running:
         if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
             running = False
  
-    # Move the player if an arrow key is pressed
+    # untuk memeriksa tombol yang ditekan userd
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT]:
         sign = 0
@@ -450,18 +470,18 @@ while running:
         # pygame.quit()
         # sys.exit()
     
-    # Calculate elapsed time
+    # menghitung waktu yang telah berlalu
     current_time = time.time()
     elapsed_time = current_time - start_time
 
-    # Check if the maximum play time is reached
+    # Periksa apakah waktu putar maksimum tercapai
     if elapsed_time >= MAX_PLAY_TIME:
         game_sound.stop_sound_effect("play")
         final_page("img/lose.png", "Sorry, You Lose!")
           
-    # Draw the scene
+    # untuk menggambar elemen-elemen dalam game pada layar
     screen.fill((213, 206, 163))
-    #screen.fill((0, 0, 0))
+    
     for wall in Walls:
          screen.blit(wall.resized, (wall.x, wall.y))
     for road in Roads:
@@ -471,11 +491,10 @@ while running:
     screen.blit(enemy.resized, (enemy.x, enemy.y))
     screen.blit(player.resized, (player.x, player.y))
     
-    # Display the timer
+    # untuk menampilkan timer
     display_timer(screen, elapsed_time)
     
     pygame.display.flip()
     clock.tick(360)
-    # game_sound.stop_all_sound_effects()
  
 pygame.quit()
