23f7aa03647fbdc048a58262f27909e26fa27d5c
yusnaaaaa <91377793+yusnaaaaa@users.noreply.github.com>
2023-05-23 00:22:31 +0700

    feat: add comment

--- sound.py
+++ sound.py
@@ -1,42 +1,34 @@
 import pygame
 import pygame.mixer
 
+# class untuk memutar dan memainkan efek suara dalam game
 class SoundEffectGame:
+    
+    # inisialisasi objek
     def __init__(self):
         pygame.mixer.init()
         self.sound_effects = {}
-    
+        
+    # untuk memuat efek suara 
     def load_sound_effect(self, name, filepath):
         sound = pygame.mixer.Sound(filepath)
         self.sound_effects[name] = sound
-    
+        
+    # untuk memutar / memainkan efek suara
     def play_sound_effect(self, name):
         sound = self.sound_effects.get(name)
         if sound:
             sound.play()
-
+            
+    # untuk menghentikan pemutaran efek suara
     def stop_sound_effect(self, name):
         if name in self.sound_effects:
             sound_effect = self.sound_effects[name]
             sound_effect.stop()
-
+            
+    # untuk menghentikan semua pemutaran efek suara yang sedang diputar dalam game
     def stop_all_sound_effects(self):
         pygame.mixer.stop()
         for sound_effect in self.sound_effects.values():
             sound_effect.stop()
 
-# # Contoh penggunaan
-# game_sound = SoundEffectGame()
-
-# # Memuat efek suara
-# game_sound.load_sound_effect("explosion", "sounds/explosion.wav")
-# game_sound.load_sound_effect("powerup", "sounds/powerup.wav")
-
-# # Memainkan efek suara
-# game_sound.play_sound_effect("explosion")
-
-# # Menghentikan efek suara
-# game_sound.stop_sound_effect("explosion")
-
-# # Menghentikan semua efek suara
-# game_sound.stop_all_sound_effects()
