633e6ca1d6cc9a84ef77835a9bda2b3d20417e03
yusnaaaaa <91377793+yusnaaaaa@users.noreply.github.com>
2023-05-22 23:35:58 +0700

    feat: add comment

--- button.py
+++ button.py
@@ -1,6 +1,8 @@
 import pygame
 
+# Class button berfungsi untuk merepresentasikan tombol dalam game
 class Button():
+    # berfungsi untuk menginisialisasi atribut-atribut objek `button` saat objek dibuat
     def __init__(self, image, pos):
         self.image = image
         self.x_pos = pos[0]
@@ -9,10 +11,12 @@ class Button():
             self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
         else:
             self.rect = pygame.Rect(self.x_pos, self.y_pos, 0, 0)
-
+            
+    # berfungsi untuk menggambar tombol pada layar
     def update(self, screen):
         if self.image is not None:
             screen.blit(self.image, self.rect)
-
+            
+    # berfungsi untuk memeriksa apakah tombol ditekan berdasarkan posisi klik user
     def checkForInput(self, position):
         return self.rect.collidepoint(position)
